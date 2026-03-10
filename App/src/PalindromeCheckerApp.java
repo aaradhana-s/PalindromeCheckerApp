8c7213ce59f444187d0f6aab8ce7ba05f52b2db6
aaradhana-s <aaradhana1025@gmail.com>
2026-03-10 15:47:19 +0530

    uc2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,7 +1,33 @@
+// Main class
 public class PalindromeCheckerApp {
-    public static void main (String args []){
-        System.out.println("welcome to palindrome checker app and management system");
+
+    // Entry point of the program
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Flag to check palindrome
+        boolean isPalindrome = true;
+
+        int left = 0;
+        int right = word.length() - 1;
+
+        // Compare characters from both ends
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
     }
-
-
 }
